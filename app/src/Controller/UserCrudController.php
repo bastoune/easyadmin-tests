adb244f1f2e8866015dcfeaeda51f0ddd20c7393
Bastien Sander <bastien@heyliot.com>
2021-10-29 16:22:15 +0200

    [FIX] remove comments

--- app/src/Controller/UserCrudController.php
+++ app/src/Controller/UserCrudController.php
@@ -2,13 +2,7 @@
 
 namespace App\Controller;
 
-use App\Enums\CssClass;
-use App\Forms\CustomFields\PlainPasswordField;
-use App\Forms\CustomFields\UUIDField;
 use App\Entity\Core\User;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Traversable;
 
@@ -22,28 +16,10 @@ class UserCrudController extends AbstractBaseCrudController
     public function getFields(string $pageName): Traversable
     {
         yield TextField::new('id');
-//        yield TextField::new('firstName');
-//        yield TextField::new('lastName');
-//        yield TextField::new('email');
-//
-//        yield AssociationField::new('profile')->setRequired(true);
-//
-//        yield PlainPasswordField::new('password')
-//            ->hideOnIndex()
-//            ->setRequired($pageName === Crud::PAGE_NEW);
-//
-//        yield TelephoneField::new('phone');
-//
-//        yield AssociationField::new('language')->hideOnIndex();
-//        yield AssociationField::new('organization');
-//        yield AssociationField::new('apps');
+        yield TextField::new('firstName');
+        yield TextField::new('lastName');
+        yield TextField::new('email');
+
     }
 
-//    public function configureCrud(Crud $crud): Crud
-//    {
-//        return $this->getDefaultCrudConfiguration($crud)
-//            ->setSearchFields(['id', 'firstName', 'lastName', 'email', 'organization.name'])
-//            ->setHelp(Crud::PAGE_INDEX, 'Search by id, firstName, lastName, email, organization.name')
-//            ;
-//    }
 }
