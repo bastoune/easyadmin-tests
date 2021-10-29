7bc8b6500f1fa8f2444a39deac0cfd8d66e82d1d
Bastien Sander <bastien@heyliot.com>
2021-10-29 16:28:44 +0200

    [FIX] simplify at max the cru controller

--- app/src/Controller/UserCrudController.php
+++ app/src/Controller/UserCrudController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Core\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Traversable;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -13,14 +11,4 @@ class UserCrudController extends AbstractCrudController
     {
         return User::class;
     }
-
-    public function getFields(string $pageName): Traversable
-    {
-        yield TextField::new('id');
-        yield TextField::new('firstName');
-        yield TextField::new('lastName');
-        yield TextField::new('email');
-
-    }
-
 }
