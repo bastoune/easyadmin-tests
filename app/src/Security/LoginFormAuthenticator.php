010c930d19a5f10d54e1ce7ceaddebeac187f306
Bastien Sander <bastien@heyliot.com>
2021-10-29 14:47:19 +0200

    [ADD] Custom Auth

---
+++ app/src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @author Bastien SANDER <bastien@heyliot.com>
+ * @copyright 2020 Heyliot (http://www.heyliot.com)
+ */
+
+namespace App\Security;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginFormAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+    private const GENERIC_ERROR_MESSAGE = 'Email or password invalid.';
+    private const APP_ACCESS_ERROR_MESSAGE = 'You don\'t have access to this App..';
+
+    private UserRepository $userRepository;
+    private UrlGeneratorInterface $urlGenerator;
+    private CsrfTokenManagerInterface $csrfTokenManager;
+
+    public function __construct(
+        UserRepository $userRepository,
+        UrlGeneratorInterface $urlGenerator,
+        CsrfTokenManagerInterface $csrfTokenManager
+    ) {
+        $this->userRepository = $userRepository;
+        $this->urlGenerator = $urlGenerator;
+        $this->csrfTokenManager = $csrfTokenManager;
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return self::LOGIN_ROUTE === $request->attributes->get('_route') && $request->isMethod('POST');
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        // THIS IS A FAKE AUTHEN, ANY PASSWORD WILL LOGIN SUCCESSFULLY
+        $email = $request->request->get('email', '');
+        $csrfToken = $request->request->get('_csrf_token', '');
+
+        $userBadge = new UserBadge($email, function ($userIdentifier) {
+            return $this->userRepository->findOneBy(['email' => $userIdentifier]);
+        });
+
+        return new SelfValidatingPassport($userBadge, [new CsrfTokenBadge('authenticate', $csrfToken)]);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+        return $this->start($request, $exception);
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null): RedirectResponse
+    {
+        return new RedirectResponse($this->urlGenerator->generate(self::LOGIN_ROUTE));
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return new RedirectResponse('/');
+    }
+}
